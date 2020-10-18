8cedadf790dce32d743c031270cf7528e9ddce47
Ernest <ernestwisniewski2@gmail.com>
2020-10-18 11:57:53 +0200

    Resolve conflicts

--- docker/prod/karabin_docker_push.sh
+++ docker/prod/karabin_docker_push.sh
@@ -8,5 +8,5 @@ sudo docker push orgkarabin/karabin:$new_tag
 #docker tag prod_lemmy orgkarabin/karabin:v0.7.61-kbin
 #docker push orgkarabin/karabin:v0.7.61-kbin
 
-#git tag $new_tag
-#git push origin $new_tag
+git tag $new_tag
+git push origin $new_tag
